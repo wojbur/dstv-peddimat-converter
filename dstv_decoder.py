d62688fe5717710a628511eedb9c5035c72de08d
Wojciech Burman <wojbur94@gmail.com>
2023-03-04 13:44:30 +0100

    expand SteelPart class, start Hole class

--- dstv_decoder.py
+++ dstv_decoder.py
@@ -1,10 +1,21 @@
 import re
 
 class SteelPart:
-    def __init__(self, dstv_content):
+    def __init__(self, dstv_content: str):
         self.part_mark = self.get_partmatk(dstv_content)
         self.profile = self.get_profile(dstv_content)
+        self.profile_type = self.get_profile_type(dstv_content)
+        self.quantity = self.get_quantity(dstv_content)
         self.holes = self.get_holes(dstv_content)
+    
+    def __repr__(self):
+        return str({
+            "part mark": self.part_mark,
+            "profile": self.profile,
+            "profile type": self.profile_type,
+            "quantity": self.quantity,
+            "holes": self.holes
+        })
 
 
     def get_partmatk(self, dstv) -> str:
@@ -12,50 +23,88 @@ class SteelPart:
         partmark = line.strip()
         return partmark
 
-    def get_profile(self, dstv_content: str) -> str:
-        line = dstv_content.splitlines()[7]
+    def get_profile(self, dstv: str) -> str:
+        line = dstv.splitlines()[8]
         profile = line.strip()
         return profile
-
     
-    def get_holes(self, dstv_content: str) -> list:
-        holes_lines = self.get_holes_lines(dstv_content)
-        holes = [{
-        "surface": self.get_surface(hole),
-        "diameter": self.get_diameter(hole),
-        "x_distance": self.get_x_distance(hole),
-        "y_distance": float(hole[2]),
-        "slotted": True if len(hole) > 4 else False
-       } for hole in holes_lines]
+    def get_profile_type(self, dstv: str) -> str:
+        line = dstv.splitlines()[9]
+        profile_type_dstv = line.strip()
+        match profile_type_dstv:
+            case "I":
+                profile_type_peddimat = "B"
+            case "U":
+                profile_type_peddimat = "C"
+            case "M":
+                profile_type_peddimat = "T"
+            case "B":
+                profile_type_peddimat = "P"
+            case "L":
+                profile_type_peddimat = "L"
+            case _:
+                profile_type_peddimat = profile_type_dstv
+        return profile_type_peddimat
+    
+    def get_quantity(self, dstv: str) -> int:
+        line = dstv.splitlines()[7]
+        quantity = int(line.strip())
+        return quantity
+    
+    def get_holes(self, dstv: str) -> list:
+        holes_lines = self.get_holes_lines(dstv)
+        holes = [Hole(line) for line in holes_lines]
         return holes
 
-    def get_holes_lines(self, dstv_content: str) -> list:
-        BO_block_content = re.findall(r"(?<=BO\n)(.*)(?=EN)", dstv_content, re.DOTALL)[0]
+    def get_holes_lines(self, dstv: str) -> list:
+        BO_block_content = re.findall(r"(?<=BO\n)(.*)(?=EN)", dstv, re.DOTALL)[0]
+        holes_lines = re.findall(r"  [ovu].+", BO_block_content)
+        return holes_lines
+
+class Hole:
+    def __init__(self, hole_line_text: str):
+        self._hole_line = self.get_hole_line_list(hole_line_text)
+        self.surface = self.get_surface(self._hole_line)
+        self.diameter = self.get_diameter(self._hole_line)
+        self.slotted = False
+        self.slot_x = 0
+        self.slot_y = 0
+    
+    def __repr__(self):
+        return str({
+            "surface": self.surface,
+            "diameter": self.diameter
+        })
+    
+    def get_hole_line_list(self, line_text: str):
+        hole_line = line_text.split()
+        return hole_line
+    
+    def get_surface(self, hole_line: list) -> str:
+        match hole_line[0]:
+            case "v":
+                return "front"
+            case "o":
+                return "top"
+            case "u":
+                return "bottom"
+            
+    def get_diameter(self, hole_line: list):
+        diameter_string = hole_line[3]
+        diameter = round(float(diameter_string)*10)
+        return diameter
+            
+
+    def get_holes_lines(self, dstv: str) -> list:
+        BO_block_content = re.findall(r"(?<=BO\n)(.*)(?=EN)", dstv, re.DOTALL)[0]
         holes_text = re.findall(r"  [ovu].+", BO_block_content)
         holes_lines = [line.split() for line in holes_text]
         return holes_lines
-        
-    def get_surface(self, hole: list) -> str:
-        if hole[0] == "v":
-            return "front"
-        if hole[0] == "o":
-            return "top"
-        if hole[0] == "u":
-            return "bottom"
-
-    def get_diameter(self, hole: list):
-        diameter_string = hole[3]
-        diameter = round(float(diameter_string)*10)
-        return diameter
-
-    def get_x_distance(self, hole: list):
-        x_distance_string = hole[1][:-1]
-        return x_distance_string
     
 
 
 with open("1002B.nc1", "r") as f:
     dstv_content = f.read()
 
-part = SteelPart(dstv_content)
-print(part.holes[0])
+steel_part = SteelPart(dstv_content)
+print(steel_part)
